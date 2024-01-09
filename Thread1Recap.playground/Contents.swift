9b7f42b0d8d44b34c7aa66883f7b851138bce423
Nikita Zhdanov <nzhdanov@lcs.on.ca>
2024-01-09 14:53:26 -0500

    Fully finished structure which describes a Dog

--- Thread1Recap.playground/Contents.swift
+++ Thread1Recap.playground/Contents.swift
@@ -1,3 +1,29 @@
 import Cocoa
 
-var greeting = "Hello, playground"
+struct Dog {
+    let breed: String
+    let color: String
+    
+    var heightInMetres: Double
+    var heightInCentimetres: Double {
+            return heightInMetres * 100
+        }
+    
+    var lengthInMeters: Double
+    var lengthInCentimetres: Double {
+            return lengthInMeters * 100
+        }
+    
+    var massInKilograms: Double
+    var massInGrams: Double {
+            return massInKilograms * 1000
+        }
+
+    var lengthHeightRatio: Double {
+        return lengthInMeters / heightInMetres
+    }
+    
+    let name: String
+    var favouriteFood: String
+    var favouriteToy: String
+}
