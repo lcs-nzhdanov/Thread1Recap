4ce8ba68a6ccb7df61a1ce4b76db6f5c0705e983
Nikita Zhdanov <nzhdanov@lcs.on.ca>
2024-01-09 20:50:27 -0500

    Created a Book structure and 2 instances for example

--- Thread1Recap.playground/Contents.swift
+++ Thread1Recap.playground/Contents.swift
@@ -27,3 +27,27 @@ struct Dog {
     var favouriteFood: String
     var favouriteToy: String
 }
+
+struct Book {
+    let name: String
+    let authorName: String
+    let authorSurname: String
+    
+    let numberOfPages: Int
+    
+    let heightInCentimetres: Double
+    let widthInCentimetres: Double
+    let depthInCentimetres: Double
+    
+    var areaInCentimitres: Double {
+        return heightInCentimetres * widthInCentimetres
+    }
+    var volumeInCentimitres: Double {
+        return heightInCentimetres * widthInCentimetres * depthInCentimetres
+    }
+}
+
+var calculusBook = Book(name: "Calculus for the AP", authorName: "Miranda", authorSurname: "Sullivan", numberOfPages: 1000, heightInCentimetres: 30, widthInCentimetres: 20.6, depthInCentimetres: 3.5)
+
+var belovedBook = Book(name: "Beloved", authorName: "Toni", authorSurname: "Morrison", numberOfPages: 367, heightInCentimetres: 15, widthInCentimetres: 10.5, depthInCentimetres: 3)
+
